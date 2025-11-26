4a1303608bb30c56a0e87a6de564a834010323dd
Minosanty <minosantiago17@gmail.com>
2025-11-26 15:18:40 -0300

    creacion y desarrollo de la coneccion a la BD

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 import express from "express";
-import taskRouter from "./src/routes/task.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+//RUTAS
+import authRouter from "./src/routes/auth.routes.js";
+import articleRouter from "./src/routes/article.routes.js";
 import userRouter from "./src/routes/user.routes.js";
-import {start_DB} from "./src/config/database.js"
+import articleTagsRouter from "./src/routes/article_tag.routes.js";
+import tagRouter from "./src/routes/tag.routes.js";
 
+import { startDB } from "./src/config/db.js";
+startDB();
 
-const PORT = process.env.PORT;
 const app = express();
+const PORT = process.env.PORT;
+
 app.use(express.json());
-app.use("/api", taskRouter);
-app.use("/api", userRouter);
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(cookieParser());
 
-app.use((req, res)=>{
-    res.status(404).json({errorMessage: "Direction not found."});
-});
+app.use("/", authRouter, userRouter, articleRouter, articleTagsRouter, tagRouter);
 
-start_DB().then(()=>{
-    app.listen(PORT, ()=>{
-        console.log("Servidor corriendo en http://localhost:"+PORT);
-    });
-});
-
-
-
-app.get("/users", async (req, res) => {
-    const users = await UserModel.findAll({
-        include: {
-            model: TaskModel,
-            as: "tasks",
-            attributes: ["id", "title", "description", "isComplete"]
-        }
-    });
-    res.json(users);
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
 });
---
+++ src/config/database.js
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
+export const startDB = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: false });
+    console.log("Se ha establecido con exito la conexión a la base de datos");
+  } catch (error) {
+    console.error(
+      "Ocurrio un error con la conexion de la base de datos:",
+      error
+    );
+  }
+};
