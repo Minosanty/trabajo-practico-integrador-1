a57c250cadddc58a307e53d686db063d0ee4c5d7
Minosanty <minosantiago17@gmail.com>
2025-11-26 15:40:30 -0300

    creacion de modelos article y article_tag

---
+++ src/models/article_tag.model.js
@@ -0,0 +1,37 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+import { ArticleModel } from "./article.model.js";
+import { TagModel } from "./tag.model.js";
+
+export const ArticleTagModel = sequelize.define("Article_Tag", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+});
+
+//RELACIONES
+ArticleModel.belongsToMany(TagModel, {
+  through: ArticleTagModel,
+  as: "articles",
+  foreignKey: "article_id",
+});
+
+TagModel.belongsToMany(ArticleModel, {
+  through: ArticleTagModel,
+  as: "tags",
+  foreignKey: "tag_id",
+});
+
+ArticleTagModel.belongsTo(ArticleModel, {
+  targetKey: "id",
+  foreignKey: "article_id",
+  onDelete: "CASCADE",
+});
+
+ArticleTagModel.belongsTo(TagModel, {
+  targetKey: "id",
+  foreignKey: "tag_id",
+  onDelete: "CASCADE",
+});
