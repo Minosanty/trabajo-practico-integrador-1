37bf907f519854e0266350f0a17eaec94b85b87f
Minosanty <minosantiago17@gmail.com>
2025-11-27 13:42:23 -0300

    desarrollo de las validaciones

---
+++ src/middlewares/validations/tag.validations.js
@@ -0,0 +1,111 @@
+import { body, param } from "express-validator";
+import { TagModel } from "../../models/tag.model.js";
+
+export const createTagValidations = [
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Name cannot be empty")
+    .isLength({ min: 2, max: 30 })
+    .withMessage("Name must contain at leas 2 characters and a maximum of 30")
+    .matches(/^(?=.*[A-Za-z])(?!.*\s)[A-Za-z]+$/)
+    .custom(async (name, { req }) => {
+      try {
+        const nameExisting = await TagModel.findOne({
+          where: { name: req.body.name },
+        });
+
+        if (nameExisting) {
+          return Promise.reject("There is already a tag with that name");
+        }
+      } catch (err) {
+        console.error("Error checking the viability of the name", err);
+        return Promise.reject("Error checking the viability of the name");
+      }
+    }),
+];
+
+//VALIDACIONES PARA TRAER UNA SOLA TAG
+export const getTagValidations = [
+  param("id")
+    .isInt({ gt: 0 })
+    .withMessage("The id must be a number greater than zero")
+    .custom(async (id, { req }) => {
+      try {
+        const tag = await TagModel.findOne({
+          where: { id: req.params.id },
+        });
+
+        if (!tag) {
+          return Promise.reject("There is no tag in the DB with that ID");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the tag by id", err);
+        return Promise.reject("Error checking the existency of the tag by id");
+      }
+    }),
+];
+
+//VALIDACIONES PARA ACTUALIZAR UNA TAG
+export const updateTagValidations = [
+  param("id")
+    .isInt({ gt: 0 })
+    .withMessage("The id must be a number greater than zero")
+    .custom(async (id, { req }) => {
+      try {
+        const tag = await TagModel.findOne({
+          where: { id: req.params.id },
+        });
+
+        if (!tag) {
+          return Promise.reject("There is no tag in the DB with that ID");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the tag by id", err);
+        return Promise.reject("Error checking the existency of the tag by id");
+      }
+    }),
+  body("name")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Name cannot be empty")
+    .isLength({ min: 2, max: 30 })
+    .withMessage("Name must contain at leas 2 characters and a maximum of 30")
+    .matches(/^(?=.*[A-Za-z])(?!.*\s)[A-Za-z]+$/)
+    .custom(async (name, { req }) => {
+      try {
+        const nameExisting = await TagModel.findOne({
+          where: { name: req.body.name },
+        });
+
+        if (nameExisting) {
+          return Promise.reject("There is already a tag with that name");
+        }
+      } catch (err) {
+        console.error("Error checking the viability of the name", err);
+        return Promise.reject("Error checking the viability of the name");
+      }
+    }),
+];
+
+//VALIDACIONES PARA ELIMINAR UNA TAG
+export const deleteTagValidations = [
+  param("id")
+    .isInt({ gt: 0 })
+    .withMessage("The id must be a number greater than zero")
+    .custom(async (id, { req }) => {
+      try {
+        const tag = await TagModel.findOne({
+          where: { id: req.params.id },
+        });
+
+        if (!tag) {
+          return Promise.reject("There is no tag in the DB with that ID");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the tag by id", err);
+        return Promise.reject("Error checking the existency of the tag by id");
+      }
+    }),
+];
---
+++ src/middlewares/validations/user.validations.js
@@ -0,0 +1,251 @@
+import { body, param } from "express-validator";
+import { UserModel } from "../../models/user.model.js";
+
+//VALIDACIONES PARA CREAR UN USUARIO Y PERFIL
+export const createUserValidations = [
+  //VALIDACIONES PARA LOS CAMPOS DEL USUARIO
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username cannot be empty")
+    .isString()
+    .withMessage("Username must be a string")
+    .isLength({ min: 3, max: 20 })
+    .withMessage(
+      "Username must contain at least 3 characters and a maximun of 20"
+    )
+    .matches(/^[a-zA-Z0-9]/)
+    .withMessage("Username must be alphanumeric")
+    .custom(async (username, { req }) => {
+      try {
+        const usernameExisting = await UserModel.findOne({
+          where: { username: req.body.username },
+        });
+
+        if (usernameExisting) {
+          return Promise.reject("Username already exists, try another one");
+        }
+      } catch (err) {
+        console.error(
+          "Error while checking the viability of the username",
+          err
+        );
+        return Promise.reject(
+          "Error while checking the viability of the username"
+        );
+      }
+    }),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email cannot be empty")
+    .isEmail()
+    .withMessage("The email format is not valid")
+    .custom(async (email, { req }) => {
+      try {
+        const emailExisting = await UserModel.findOne({
+          where: { email: req.body.email },
+        });
+
+        if (emailExisting) {
+          return Promise.reject("That email already exists, try another one");
+        }
+      } catch (err) {
+        console.error("Error checking the viability of the email", err);
+        return Promise.reject("Error checking the viability of the email");
+      }
+    }),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password cannot be empty")
+    .isString()
+    .withMessage("Password must be a string")
+    .isLength({ min: 8 })
+    .withMessage("Password must contain at least 8 characters")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/),
+  body("role")
+    .trim()
+    .notEmpty()
+    .withMessage("Role cannot be empty")
+    .isIn(["user", "admin"])
+    .withMessage("Role must be either 'user' or 'admin'"),
+  //VALIDACIONES PARA LOS CAMPOS DEL PERFIL
+  body("first_name")
+    .trim()
+    .notEmpty()
+    .withMessage("First_name cannot be empty")
+    .isString()
+    .withMessage("First_name must be a string")
+    .isLength({ min: 2, max: 50 })
+    .withMessage(
+      "First_name must contain at least 5 characters and a maximum of 50"
+    )
+    .matches(/^[A-Za-z]+$/)
+    .withMessage("First_name must contain only letters, not numbers or signs"),
+  body("lastname")
+    .trim()
+    .notEmpty()
+    .withMessage("Lastname cannot be empty")
+    .isString()
+    .withMessage("Lastname must be a string")
+    .isLength({ min: 2, max: 50 })
+    .withMessage(
+      "Lastname must contain at least 5 characters and a maximum of 50"
+    )
+    .matches(/^[A-Za-z]+$/)
+    .withMessage("Lastname must contain only letters, not numbers or signs"),
+  body("biography")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Biography cannot be empty")
+    .isString()
+    .withMessage("Biography must be a string")
+    .isLength({ max: 500 })
+    .withMessage("Biography cannot exceed 500 characters"),
+  body("avatar_url")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("The avatar_url cannot be empty")
+    .isURL()
+    .withMessage("The avatar_url format is invalid"),
+  body("birth_date")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Birth_date cannot be empty"),
+];
+
+//VALIDACIONES PARA TRAER UN SOLO USUARIO
+export const getUserValidations = [
+  param("id")
+    .isInt({ gt: 0 })
+    .withMessage("The id must be a number greater than 0")
+    .custom(async (id, { req }) => {
+      try {
+        const userExisting = await UserModel.findOne({
+          where: { id: req.params.id },
+        });
+
+        if (!userExisting) {
+          return Promise.reject("There is no user in the DB with that id");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the user by id", err);
+        return Promise.reject("Error checking the existency of the user by id");
+      }
+    }),
+];
+
+//VALIDACIONES PARA ACTUALIZAR UN USUARIO
+export const updateUerValidations = [
+  body("username")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Username cannot be empty")
+    .isString()
+    .withMessage("Username must be a string")
+    .isLength({ min: 3, max: 20 })
+    .withMessage(
+      "Username must contain at least 3 characters and a maximun of 20"
+    )
+    .matches(/^[a-zA-Z0-9]/)
+    .withMessage("Username must be alphanumeric")
+    .custom(async (username, { req }) => {
+      try {
+        const usernameExisting = await UserModel.findOne({
+          where: { username: req.body.username },
+        });
+
+        if (usernameExisting) {
+          return Promise.reject("Username already exists, try another one");
+        }
+      } catch (err) {
+        console.error(
+          "Error while checking the viability of the username",
+          err
+        );
+        return Promise.reject(
+          "Error while checking the viability of the username"
+        );
+      }
+    }),
+  body("email")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Email cannot be empty")
+    .isEmail()
+    .withMessage("The email format is not valid")
+    .custom(async (email, { req }) => {
+      try {
+        const emailExisting = await UserModel.findOne({
+          where: { email: req.body.email },
+        });
+
+        if (emailExisting) {
+          return Promise.reject("That email already exists, try another one");
+        }
+      } catch (err) {
+        console.error("Error checking the viability of the email", err);
+        return Promise.reject("Error checking the viability of the email");
+      }
+    }),
+  body("password")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Password cannot be empty")
+    .isString()
+    .withMessage("Password must be a string")
+    .isLength({ min: 8 })
+    .withMessage("Password must contain at least 8 characters")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/),
+  body("role")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Role cannot be empty")
+    .isIn(["user", "admin"])
+    .withMessage("Role must be either 'user' or 'admin'"),
+  param("id")
+    .isInt({ gt: 0 })
+    .withMessage("The id must be a number greater than 0")
+    .custom(async (id, { req }) => {
+      try {
+        const userExisting = await UserModel.findOne({
+          where: { id: req.params.id },
+        });
+
+        if (!userExisting) {
+          return Promise.reject("There is no user in the DB with that id");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the user by id", err);
+        return Promise.reject("Error checking the existency of the user by id");
+      }
+    }),
+];
+
+export const deleteUserValidations = [
+  param("id")
+    .isInt({ gt: 0 })
+    .withMessage("The id must be a number greater than 0")
+    .custom(async (id, { req }) => {
+      try {
+        const userExisting = await UserModel.findOne({
+          where: { id: req.params.id },
+        });
+
+        if (!userExisting) {
+          return Promise.reject("There is no user in the DB with that id");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the user by id", err);
+        return Promise.reject("Error checking the existency of the user by id");
+      }
+    }),
+];
